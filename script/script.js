16f8f1a9435a096cc7fc1207175129b216b0ab4a
AnthonyMAURICE <116059905+AnthonyMAURICE@users.noreply.github.com>
2024-06-26 12:28:35 +0200

    feat: Calcul fonctions done, still need to optimize

--- script/script.js
+++ script/script.js
@@ -1,6 +1,7 @@
 import {Question} from "../Question.js"
 let counter = 0
-    
+let resultArray = []
+
 const dataUrl = "../data/data.json"
 const current = document.getElementById('current-question')
 const total = document.getElementById('total-questions')
@@ -9,7 +10,7 @@ const btns = document.getElementById('btn-wrapper')
 const prec = document.getElementById('prec')
 const next = document.getElementById('next')
 const sub = document.getElementById('sub')
-
+const body = document.querySelector('body')
 
 async function getData(_url) {
     const response = await fetch(_url);
@@ -53,8 +54,48 @@ next.addEventListener('click', () =>{
 })
 
 sub.addEventListener('click', () => {
-    alert('SCORE: Osef lol   ')
+    resultArray.push('A: ' + calculateA(), 'C: ' + calculateC(),'L: ' + calculateL(),'D: ' + calculateD(),'S: ' + calculateS(),'S2: ' + calculateSBis(), 'R: ' + calculateR())
+    formulaire.remove()
+    for(let i = 0; i < resultArray.length; i++){
+        const p = document.createElement('p')
+        p.textContent = resultArray[i]
+        body.appendChild(p)
+    }
+    const btnReturn = document.createElement('button')
+    btnReturn.textContent = 'Retour'
+    btnReturn.addEventListener('click', () => {
+        location.reload()
+    })
+    body.appendChild(btnReturn)
 })
+
+function calculateA(){
+    return 4*(questions[3].value + (5-questions[5].value) + questions[7].value)
+}
+
+function calculateC(){
+    return 2*(questions[0].value + (5 - questions[1].value)+ questions[2].value + questions[4].value + questions[6].value +questions[8].value)
+}
+
+function calculateL(){
+    return calculateA() + calculateC()
+}
+
+function calculateD(){
+    return questions[9].value +questions[10].value +questions[11].value + (5-questions[12].value) + questions[13].value +questions[14].value +questions[15].value + questions[16].value +questions[17].value
+}
+
+function calculateS(){
+    return questions[18].value +questions[19].value +questions[20].value +questions[21].value
+}
+
+function calculateSBis(){
+    return questions[22].value +questions[23].value +questions[24].value +questions[25].value
+}
+
+function calculateR(){
+    return (5-questions[26].value) + (5-questions[27].value) + questions[28].value +questions[29].value + questions[30].value + questions[31].value
+}
 
 function display(_counter){
     const question = questions[_counter]
